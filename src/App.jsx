b78228f24e1af6b9f76a6da77be7b338753788b1
Your Nameemail <shareef.99997@gmail.com>
2023-09-02 18:14:21 +0300

    added imports.js

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,6 @@
 import './App.css';
-import Header from './Pages/Header/Header';
-import About from './Pages/About/About';
-import Contact from './Pages/Contact/Contact';
-import Experiance from './Pages/Experiance/Experiance';
-import Footer from './Pages/Footer/Footer';
-import Home from './Pages/Home/Home';
-import Projects from './Pages/Projects/Projects';
+import { Header, About, Contact, Experiance, Footer, Home, Projects } from './Pages/imports';
+
 
 function App() {
   return (
---
+++ src/Pages/imports.js
@@ -0,0 +1,9 @@
+import Header from './Header/Header';
+import About from './About/About';
+import Contact from './Contact/Contact';
+import Experiance from './Experiance/Experiance';
+import Footer from './Footer/Footer';
+import Home from './Home/Home';
+import Projects from './Projects/Projects';
+
+export { Header, About, Contact, Experiance, Footer, Home, Projects };
