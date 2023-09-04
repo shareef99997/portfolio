951c79ba686e54bd9cf689f05eff979af6fe3bbf
Your Nameemail <shareef.99997@gmail.com>
2023-09-04 19:55:42 +0300

    finished ui

--- src/Pages/Contact/Contact.jsx
+++ src/Pages/Contact/Contact.jsx
@@ -1,19 +1,37 @@
-import React from 'react'
+import {React ,useRef} from 'react'
 import './Contact.css'
+import emailjs from '@emailjs/browser';
+ 
 const Contact=()=>{
+
+  const form = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs.sendForm('shareef_portfolio', 'template_9lsgkx7', form.current, 'VONWVF5lDExBpxU1f')
+      .then((result) => {
+          console.log(result.text);
+          alert('message sent seccessfully 😄');
+      }, (error) => {
+          console.log(error.text);
+      });
+  };
+
+
     return (
       <section class="contact" id="contact">
         <h2 class="heading">Contact <span>Me!</span></h2>
-        <form action="#">
+        <form ref={form} onSubmit={sendEmail}>
             <div class="input-box">
-                <input type="text" placeholder="Full Name"/>
-                <input type="email" placeholder="Email Address"/>
+                <input type="text" name='user_name' placeholder="Full Name"/>
+                <input type="email" name='user_email' placeholder="Email Address"/>
             </div>
             <div class="input-box">
-                <input type="number" placeholder="Mobile Number"/>
-                <input type="text" placeholder="Email Subject"/>
+                <input type="number" name='user_phone' placeholder="Mobile Number"/>
+                <input type="text" name='subject' placeholder="Email Subject"/>
             </div>
-            <textarea name="" id="" cols="30" rows="10" placeholder="Your Message"></textarea>
+            <textarea name="message" id="" cols="30" rows="10" placeholder="Your Message"></textarea>
             <input type="submit" value="Send Message" class="btn"/>
             </form>
     </section>
--- src/Pages/Footer/Footer.jsx
+++ src/Pages/Footer/Footer.jsx
@@ -1,8 +1,16 @@
 import React from 'react'
-
+import './Footer.css';
 const Footer=()=> {
     return (
-      <div>Footer</div>
+      <footer class="footer">
+        <div class="footer-text">
+            <p>Copyright &copy; 2023 by Codehal | All Rights Reserved.</p>
+        </div>
+
+        <div class="footer-iconTop">
+            <a href="#home"><i class='bx bx-up-arrow-alt'></i></a>
+        </div>
+    </footer>
     )
   }
 
