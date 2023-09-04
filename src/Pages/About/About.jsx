0ae97f6da02e17f6b7f8fffb8db90e8e9d4ce97a
Your Nameemail <shareef.99997@gmail.com>
2023-09-04 18:07:23 +0300

    finished projects

--- src/Pages/About/About.jsx
+++ src/Pages/About/About.jsx
@@ -11,7 +11,7 @@ const About=()=>{
         <div class="about-content">
             <h2 class="heading">About <span>Me</span></h2>
             <h3>Frontend Developer!</h3>
-            <p>I am a fresh Graduate with a bacholor of Information Technology and a frontend developer and UI UX designer who loves creating beautiful and user-friendly web experiences. I have practicle experience in using HTML, CSS, JavaScript, React, Bootstrap, Figma, to design and develop websites and web applications for various domains. I am passionate about solving problems, learning new things, and collaborating with other developers and designers. I also have a keen eye for design and get inspired by art, animation. I am ambitious and always looking for new challenges and opportunities to grow as a professional.</p>
+            <p>I am a fresh graduate with a bachelor’s degree in Information Technology , and i would discribe myself as a frontend developer and UI UX designer who creates beautiful and user-friendly web experiences. I have practical experience in using HTML, CSS, JavaScript, React, Bootstrap, Figma, etc. to design and develop websites and web applications for various domains. I am passionate about solving problems, learning new things, and collaborating with other developers and designers. I also have a keen eye for design and get inspired by art and animation. I am ambitious and always looking for new challenges and opportunities to grow as a professional.</p>
             <a href="#" class="btna">Read More</a>
         </div>
     </section>
--- src/Pages/Projects/Projects.jsx
+++ src/Pages/Projects/Projects.jsx
@@ -1,8 +1,93 @@
 import React from 'react'
+import './Projects.css'
 
 const Projects=()=> {
     return (
-      <div>Projects</div>
+      <section class="Projects" id="Projects">
+        <h2 class="heading">Latest <span>Projects</span></h2>
+
+        <div class="Projects-container">
+            {/*/////////////// QPT ///////////////*/}
+            <div class="Projects-box">
+                <img className='project-img' src={require('../../assets/qpt.jpeg')} alt=""/>
+                <div class="Projects-layer">
+                    <h4>QPT</h4>
+                    <div class ="l">
+                    <img class="tool" src={require('../../assets/react.png')} alt=""/>
+                    </div>
+                    <p>AI Services Frontend design</p>
+                    <a href="https://shareef99997.github.io/QPT/" target="_blank"><i class='bx bx-link-external'></i></a>
+                </div>
+            </div>
+            {/*/////////////// Q-Bank ///////////////*/}
+            <div class="Projects-box">
+                <img className='project-img' src={require('../../assets/qbank.jpeg')} alt=""/>
+                <div class="Projects-layer">
+                    <h4>Q-Bank</h4>
+                    <div className='l'><img class="tool" src={require('../../assets/react.png')} alt=""/></div>
+                    
+                    <p>Credit Card Services Company Frontend design</p>
+                    <a href="https://qbankq.netlify.app/" target="_blank"><i class='bx bx-link-external'></i></a>
+                </div>
+            </div>
+            {/*/////////////// E-Store ///////////////*/}
+            <div class="Projects-box">
+                <img className='project-img' src={require('../../assets/estore.jpeg')} alt=""/>
+                <div class="Projects-layer">
+                    <h4>Web Design</h4>
+                    <div className='l'>
+                        <img class="tool" src={require('../../assets/html.png')} alt=""/>
+                        <img class="tool" src={require('../../assets/css.png')} alt=""/>
+                        <img class="tool" src={require('../../assets/javascript.png')} alt=""/>
+                    </div>
+                    <p>a responsive E-Store design</p>
+                    <a href="https://shareef99997.github.io/E-Store/" target="_blank"><i class='bx bx-link-external'></i></a>
+                </div>
+            </div>
+            {/*/////////////// Semsar ///////////////*/}
+            <div class="Projects-box">
+                <img className='project-img' src={require('../../assets/Semsar.png')} alt=""/>
+                <div class="Projects-layer">
+                    <h4>Web Design</h4>
+                    <div className='l'>
+                        <img class="tool" src={require('../../assets/flutter.png')} alt=""/>
+                        <img class="tool" src={require('../../assets/firebase.png')} alt=""/>
+                    </div>
+                    <p>a complete mobile app for house services</p>
+                    <a href="https://play.google.com/store/apps/details?id=com.shareef.Semsar&pli=1" target="_blank"><i class='bx bx-link-external'></i></a>
+                </div>
+            </div>
+            {/*/////////////// Info-App ///////////////*/}
+            <div class="Projects-box">
+                <img className='project-img' src={require('../../assets/info.jpeg')} alt=""/>
+                <div class="Projects-layer">
+                    <h4>Web Design</h4>
+                    <div className='l'>
+                        <img class="tool" src={require('../../assets/html.png')} alt=""/>
+                        <img class="tool" src={require('../../assets/css.png')} alt=""/>
+                        <img class="tool" src={require('../../assets/javascript.png')} alt=""/>
+                    </div>
+                    <p>multible small apps using Api</p>
+                    <a href="https://shareef99997.github.io/Info_App/" target="_blank"><i class='bx bx-link-external'></i></a>
+                </div>
+            </div>
+            {/*/////////////// Youtube Clone ///////////////*/}
+            <div class="Projects-box">
+                <img className='project-img' src={require('../../assets/youtube.jpeg')} alt=""/>
+                <div class="Projects-layer">
+                    <h4>Web Design</h4>
+                    <div className='l'>
+                        <img class="tool" src={require('../../assets/html.png')} alt=""/>
+                        <img class="tool" src={require('../../assets/css.png')} alt=""/>
+                        <img class="tool" src={require('../../assets/javascript.png')} alt=""/>
+                    </div>
+                    <p>Humbly My first Project in web development</p>
+                    <a href="https://shareef99997.github.io/Youtube-clone/" target="_blank"><i class='bx bx-link-external'></i></a>
+                </div>
+            </div>
+
+        </div>
+    </section>
     )
   }
 
