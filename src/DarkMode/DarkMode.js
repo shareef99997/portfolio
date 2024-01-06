3cfcf675be324e6c064b6c65ad95f1889e200bfd
Your Nameemail <shareef.99997@gmail.com>
2024-01-06 18:09:54 +0300

    set theme mode

--- src/DarkMode/DarkMode.js
+++ src/DarkMode/DarkMode.js
@@ -4,12 +4,36 @@ import { ReactComponent as Moon } from "./Moon.svg";
 import "./DarkMode.css";
 
 const DarkMode = () => {
+
+    const setDarkMode = () => {
+        document.querySelector("body").setAttribute('data-theme', 'dark');
+        localStorage.setItem("selectedTheme", "dark");
+    }
+    const setLightMode = () => {
+        document.querySelector("body").setAttribute('data-theme', 'light');
+        localStorage.setItem("selectedTheme", "light");
+    }
+
+    const selectedTheme = localStorage.getItem('selectedTheme');
+
+    if (selectedTheme === 'dark') {
+        setDarkMode();
+    }else{
+        setLightMode();
+    }
+
+    const toggleTheme = (e) => {
+        if (e.target.checked) setDarkMode();
+        else setLightMode();
+    }
     return (
         <div className='dark_mode'>
             <input
                 className='dark_mode_input'
                 type='checkbox'
                 id='darkmode-toggle'
+                onChange={toggleTheme}
+                defaultChecked={selectedTheme === "dark"} 
             />
             <label className='dark_mode_label' for='darkmode-toggle'>
                 <Sun />
