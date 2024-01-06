1ffca71a2f03abf5fc9060ab13812418b4004630
Your Nameemail <shareef.99997@gmail.com>
2024-01-06 16:59:26 +0300

    set light and dark mode colors

--- src/Pages/About/About.jsx
+++ src/Pages/About/About.jsx
@@ -12,7 +12,7 @@ const About=()=>{
             <h2 class="heading">About <span>Me</span></h2>
             <h3>Frontend Developer!</h3>
             <p>I am a fresh graduate with a bachelor’s degree in Information Technology , and i would discribe myself as a frontend developer and Mobile apps developer who creates beautiful and user-friendly websites ,Mobile apps . I have practical experience in using HTML, CSS, JavaScript, React, Bootstrap, Figma,flutter,firebase, etc. to design and develop websites and Mobile applications for various domains. I am passionate about solving problems, learning new things, and collaborating with other developers and designers. I also have a keen eye for design and get inspired by art and animation. I am ambitious and always looking for new challenges and opportunities to grow as a professional.</p>
-            <a href="#" class="btna">Read More</a>
+            <a href="#" class="btn">Read More</a>
         </div>
     </section>
     )
