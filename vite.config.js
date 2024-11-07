c0c925337dadfadd2451d331e45b0d5a540fbdbe
Shareef Ali <Shareeff@taheiya.sa>
2024-11-07 14:02:20 +0300

    test deploy to github pages

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
+  base: '/', // Set to '/' if using a custom domain directly
   plugins: [react()],
 })
