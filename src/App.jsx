5817bcc3af624b19df65a3baeda39d1c3ac005fa
Your Nameemail <shareef.99997@gmail.com>
2024-11-07 23:48:53 +0300

    set custom colors and font for website

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,17 @@
 function App() {
   return (
     <>
-      <h1 className="text-3xl text-white">My Name is Shareef</h1>
+    <div>
+      <div className=""><h1 className="text-3xl ">My Name is Shareefgg </h1></div>
+
+      <p className=" ">
+        Incididunt aliquip ad aute elit non in 
+      </p>
+      <p className="text-lg ttext-light ">
+        Incididunt aliquip ad aute elit non in laboris ullamco. Consectetur aute labore duis sit Lorem. Velit proident nisi cupidatat qui sint veniam magna incididunt incididunt quis reprehenderit aliqua do commodo.
+      </p>
+
+    </div>
     </>
   )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,24 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'primary': '#89DB7E',
+        'text': '#e9e9f1',
+        'text-light': '#aeaeae',
+        'background': '#29292a',
+      },
+      backgroundImage: {
+        'primary-gradient': 'linear-gradient(180deg, #89DB7E 0%, #66E778 100%)',
+        'background-gradient': 'linear-gradient(135deg, #29292a 0%, #181818 100%)',
+      },
+      fontSize: {
+        'Headline': '4.875rem',   
+        'Header': '3rem',             
+        'title': '1.5rem',            
+        'description': '0.875rem',             
+      },
+    },
   },
   plugins: [],
 }
