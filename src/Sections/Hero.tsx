9459f3bc0903989c56e1efb39166ec6bba32cbbb
Shareef-taheiya <shareeff@taheiya.sa>
2025-06-15 14:50:34 +0300

    Rename Assets folder name

--- src/Sections/Hero.tsx
+++ src/Sections/Hero.tsx
@@ -1,7 +1,7 @@
 import { motion, AnimatePresence } from "framer-motion";
 import { useEffect, useState } from "react";
 import { useInView } from "react-intersection-observer";
-import Resume from "../Assets/Credentials/Shareef's Resume.pdf";
+import Resume from "../Assets/Credentials/Resume/Shareef's Resume.pdf";
 import { PrimaryButton, SecondaryButton } from "../components/buttons";
 import { BarChart, Code2, LineChart, Sparkles } from "lucide-react";
 import SectionHeader from "../components/SectionHeader";
